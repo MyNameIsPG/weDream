f7e760fc7fa510e793bc28101d9a899300053dd3
MyNamePG <493871850@qq.com>
2018-10-05 22:14:34 +0800

    xing

--- src/components/grid/gridIndexItem.vue
+++ src/components/grid/gridIndexItem.vue
@@ -1,15 +1,58 @@
 <template>
 	<div class="gridIndexItem">
-    <div>
-      <h2>网格点概况</h2>
+    <div class="gridBox">
+      <div class="gridBox-head"><h2>网格点概况</h2></div>
+      <div class="gridBox-body">
+
+      </div>
+    </div>
+    <div class="gridBox">
+      <div class="gridBox-head"><h2>网格点动态 <span>更多></span></h2></div>
+      <div class="gridBox-body">
+        <post></post>
+      </div>
+    </div>
+    <div class="gridBox">
+      <div class="gridBox-head"><h2>网格点红黑榜 <span>更多></span></h2></div>
+      <div class="gridBox-body">
+        <post></post>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-	export default {
-		name: "gridIndexItem"
-	}
+import post from '@/components/post/post';
+export default {
+  name: "gridIndexItem",
+  components: {
+    post
+  },
+  data(){
+    return {
+      dataList: [],
+      postQueryData: [],
+    }
+  },
+  mounted(){
+    //this.circleQuery();
+  },
+  methods: {
+    //分页查询圈子
+    circleQuery(){
+      let params = {
+        pageNum: 1,
+        pageSize: 7,
+        communityId: sessionStorage.getItem("communityId"),
+      }
+      circleQuery(params).then(data => {
+        if (data.data.code == 200) {
+          this.dataList = data.data.data.list
+        }
+      })
+    },
+  }
+}
 </script>
 
 <style scoped>
---
+++ src/components/post/post.vue
@@ -0,0 +1,35 @@
+<template>
+	<ul class="post">
+    <li>
+      <img src="../../assets/img/bg/bg1.jpg" alt="">
+      <div class="">
+        <h2>融雪除病为民解困，携手共创和谐社会</h2>
+        <p>2018年2月5日下午，湘湖管理局跃进湖社区为芙蓉区消防大队湘湖中队和长沙</p>
+      </div>
+    </li>
+    <li>
+      <img src="../../assets/img/bg/bg1.jpg" alt="">
+      <div class="">
+        <h2>融雪除病为民解困，携手共创和谐社会</h2>
+        <p>2018年2月5日下午，湘湖管理局跃进湖社区为芙蓉区消防大队湘湖中队和长沙</p>
+      </div>
+    </li>
+    <li>
+      <img src="../../assets/img/bg/bg1.jpg" alt="">
+      <div class="">
+        <h2>融雪除病为民解困，携手共创和谐社会</h2>
+        <p>2018年2月5日下午，湘湖管理局跃进湖社区为芙蓉区消防大队湘湖中队和长沙</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "post"
+}
+</script>
+
+<style>
+
+</style>
